82b65a59e838917ab9cc0d0637c6cdaf611c7895
DenRebrov <32088023+DenRebrov@users.noreply.github.com>
2018-01-23 16:36:26 +0300

    Update hashtag.rb

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,5 +1,7 @@
 class Hashtag < ApplicationRecord
+  # у хэштега может быть много вопросов
   has_and_belongs_to_many :questions
 
+  # имя хэштега должно быть уникально
   validates :name, uniqueness: true
 end
