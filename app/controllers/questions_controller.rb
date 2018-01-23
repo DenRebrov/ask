43ebdb8fc91eae56802fada755cd0b2b6119df6d
DenRebrov <32088023+DenRebrov@users.noreply.github.com>
2018-01-23 16:33:51 +0300

    Update questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,26 +1,33 @@
 class QuestionsController < ApplicationController
+  # Инициализируем объект вопрос для экшенов кроме :edit, :update, :destroy
   before_action :load_question, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_user, except: [:create, :questions_hashtags_page]
 
-  def show
-  end
+  # Проверяем имеет ли юзер доступ к экшену для всей дествий, кроме задавания
+  # вопроса, это действие может вызвать даже неавторизованный пользователь.
+  before_action :authorize_user, except: [:create]
 
-  def edit
-  end
+  # Перед этим действием сработает before_action :load_questions и в переменной
+  # @question у нас будет лежать вопрос с нужным id равным params[:id].
+  def edit; end
 
+  # Действие create будет отзываться при POST-запросе по адресу /questions из
+  # формы нового вопроса, которая находится в шаблоне на странице
+  # /questions/edit
   def create
     @question = Question.new(question_params)
 
     current_user.present? ? @question.author = current_user : @question.author = nil
 
-    #if verify_recaptcha(model: @question) && @question.save
-    if @question.save
+    # проверка recaptcha на интернет-бота
+    if verify_recaptcha(model: @question) && @question.save
       redirect_to user_path(@question.user), notice: "Вопрос задан."
     else
       render :edit
     end
   end
 
+  # Перед этим действием сработает before_action :load_questions и в переменной
+  # @question у нас будет лежать вопрос с нужным id равным params[:id].
   def update
     if @question.update(question_params)
       redirect_to user_path(@question.user), notice: 'Вопрос сохранен.'
@@ -29,10 +36,12 @@ class QuestionsController < ApplicationController
     end
   end
 
+  # Удаляем вопрос
   def destroy
     user = @question.user
     @question.destroy
 
+    # Редиректим пользователя на страницу пользователя
     redirect_to user_path(user), notice: 'Вопрос удален.'
   end
 
